cc8d791a922ee9e56f7ba9f9b3f9f3a50dd10c0d
muraikenta <muraikenta0507@gmail.com>
2022-11-30 16:15:02 +0900

    update map

--- src/soundData.ts
+++ src/soundData.ts
@@ -4,14 +4,16 @@ const soundData: { [index: string]: SoundProps } = {
     eating: {
         // "Chewing" by InspectorJ - freesound.org
         src: './assets/sfx/eating.wav',
+        volume: 0.5,
     },
     drinking: {
         // "Drinking" by dersuperanton - freesound.org"
         src: './assets/sfx/drinking.wav',
+        volume: 0.5,
     },
     footstep: {
         src: './assets/sfx/footstep.wav',
-        volume: 0.75,
+        volume: 0.1,
     },
 };
 
